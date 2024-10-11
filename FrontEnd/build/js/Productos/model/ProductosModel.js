e8781029f1e0885d806f5f04875a53d0215839bf
karenp2904 <karen2005yulieth@gmail.com>
2024-10-11 10:38:20 -0500

    host admin inventario

--- FrontEnd/build/js/Productos/model/ProductosModel.js
+++ FrontEnd/build/js/Productos/model/ProductosModel.js
@@ -13,10 +13,11 @@ export default class ProductosModel {
         return this.productos;
     }
     async init() {
-        this.productos = await this.getProductsFetch();
+        this.productos = await this.getProductsServer();
     }
     async deleteProducto(id) {
         this.productos = this.productos.filter(producto => producto.id !== id);
+        await this.deleteProductsServer(id);
     }
     async updateProducto(updatedProducto) {
         console.log(updatedProducto);
@@ -28,9 +29,65 @@ export default class ProductosModel {
         else {
             console.error("No se encontró el producto para actualizar.");
         }
+        await this.updateProductsServer(updatedProducto);
     }
     async createProducto(newProduct) {
         console.log(newProduct);
         this.productos.push(newProduct);
+        await this.addProductsServer(newProduct);
     }
+    getProductsServer = async () => {
+        const response = await fetch('http://localhost:1802/api/v1.0/productos');
+        if (response.status !== 200) {
+            return [];
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    };
+    addProductsServer = async (producto) => {
+        const response = await fetch('http://localhost:1802/api/v1.0/productos/agregar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(producto),
+        });
+        if (response.status !== 200) {
+            return null;
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    };
+    deleteProductsServer = async (idProducto) => {
+        const response = await fetch('http://localhost:1802/api/v1.0/productos/eliminar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(idProducto),
+        });
+        if (response.status !== 200) {
+            return null;
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    };
+    updateProductsServer = async (producto) => {
+        const response = await fetch('http://localhost:1802/api/v1.0/productos/actualizar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(producto),
+        });
+        if (response.status !== 200) {
+            return null;
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    };
 }
